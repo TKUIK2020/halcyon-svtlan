23ecd00efd93e04bdcdfe8e62617dbec22decaf0
Svetlana <smorr4ckovasveta@gmail.com>
2020-04-28 16:16:32 +0400

    XHR

---
+++ lab6/xhr.js
@@ -0,0 +1,19 @@
+
+	const requestURL = "https://jsonplaceholder.typicode.com/posts"
+	const xhr = new XMLHttpRequest()
+	xhr.open("POST", requestURL)
+	xhr.onload = () =>{
+		
+		console.log(xhr.response)
+	}
+	xhr.send(JSON.stringify({title: 'foo', body: 'bar', userId: 1}))
+
+
+
+
+
+
+
+
+
+  
