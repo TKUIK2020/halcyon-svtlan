86bda0c84d167d76b12802a8f7e1753b09c4981f
Svetlana <smorr4ckovasveta@gmail.com>
2020-04-29 15:20:33 +0400

    Lab6

--- lab6/xhr.js
+++ lab6/xhr.js
@@ -1,12 +1,12 @@
 
-	const requestURL = "https://jsonplaceholder.typicode.com/posts"
-	const xhr = new XMLHttpRequest()
-	xhr.open("GET", requestURL)
+    const requestURL = "https://jsonplaceholder.typicode.com/posts"
+    const xhr = new XMLHttpRequest()
+    xhr.open("POST", requestURL)
 	xhr.onload = () =>{
 		
 		console.log(xhr.response)
 	}
-	xhr.send()
+	xhr.send(JSON.stringify({title: 'foo', body: 'bar', userId: 1}))
 
 
 
